b4dba1e4288730fdbc9e86575cfe9074ebe712d0
Erik Huang <ekhu94@gmail.com>
2021-05-05 12:53:26 -0700

    commit tree 9d6a52636d93bd7c01aafa3555982001e9527b3e

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,30 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:show]
   
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def set_student
     @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
   end
 
 end
